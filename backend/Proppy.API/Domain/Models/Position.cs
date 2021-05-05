dfe54072ff93802b9783718bde02c800b979c127
Eason Chai <echai2905@gmail.com>
2021-05-05 23:10:18 +0800

    Modify Schema

--- backend/Proppy.API/Domain/Models/Position.cs
+++ backend/Proppy.API/Domain/Models/Position.cs
@@ -4,6 +4,7 @@ namespace Proppy.API.Domain.Models
 {
     public class Position
     {
+        public int Id {get; set;}
         public string Code {get; set;}
         public string Description {get; set;}
         public ICollection<Employee> Employees {get; set;}
--- backend/Proppy.API/Persistence/Contexts/AppDbContext.cs
+++ backend/Proppy.API/Persistence/Contexts/AppDbContext.cs
@@ -15,7 +15,8 @@ namespace Proppy.API.Persistence.Contexts
 
             builder.Entity<Position>().ToTable("Positions");
             builder.Entity<Position>().HasKey(p => p.Code);
-            builder.Entity<Position>().Property(p => p.Code).HasColumnType("varchar(2)").HasMaxLength(2).IsRequired();
+            builder.Entity<Position>().Property(p => p.Id).HasColumnType("int").ValueGeneratedOnAdd();
+            builder.Entity<Position>().Property(p => p.Code).HasColumnType("varchar(2)").HasMaxLength(2).IsRequired().HasComputedColumnSql("");
             builder.Entity<Position>().Property(p => p.Description).HasColumnType("varchar(20)").HasMaxLength(20).IsRequired();
             builder.Entity<Position>().HasMany(p => p.Employees).WithOne(p => p.Position).HasForeignKey(p => p.Position_Code).IsRequired();
 
@@ -35,6 +36,7 @@ namespace Proppy.API.Persistence.Contexts
             builder.Entity<Employee>().Property(p => p.Email).HasColumnType("varchar(30)").HasMaxLength(30).IsRequired();
             builder.Entity<Employee>().Property(p => p.Gender).HasColumnType("varchar(1)").HasMaxLength(1).IsRequired();
             builder.Entity<Employee>().HasOne(p => p.Position).WithMany(p => p.Employees).HasForeignKey(p => p.Position_Code).IsRequired();
+            builder.Entity<Employee>().Property(p => p.Position_Code).HasColumnType("varchar(1)").HasMaxLength(1);
             builder.Entity<Employee>().Property(p => p.DOB).HasColumnType("date").HasMaxLength(16).IsRequired();
             builder.Entity<Employee>().Property(p => p.Salary).HasColumnType("decimal(16)").HasMaxLength(16).IsRequired();
             builder.Entity<Employee>().Property(p => p.Remarks).HasColumnType("varchar(1000)").HasMaxLength(1000);
