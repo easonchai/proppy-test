2dbbb253c697e2028bd509d4c8fdd8dbf7335e4c
Eason Chai <echai2905@gmail.com>
2021-05-06 14:11:57 +0800

    Modify Employee Entity

--- backend/Proppy.API/Persistence/Contexts/AppDbContext.cs
+++ backend/Proppy.API/Persistence/Contexts/AppDbContext.cs
@@ -31,18 +31,21 @@ namespace Proppy.API.Persistence.Contexts
 
             builder.Entity<Employee>().ToTable("Employees");
             builder.Entity<Employee>().HasKey(p => p.ID);
-            builder.Entity<Employee>().Property(p => p.ID).HasColumnType("bigint").HasMaxLength(19).IsRequired();
+            builder.Entity<Employee>().Property(p => p.ID).HasColumnType("bigint").HasMaxLength(19).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<Employee>().HasIndex(p => p.ID).IsUnique();
             builder.Entity<Employee>().Property(p => p.Name).HasColumnType("varchar(30)").HasMaxLength(30).IsRequired();
             builder.Entity<Employee>().Property(p => p.Phone_No).HasColumnType("varchar(20)").HasMaxLength(20).IsRequired();
+            builder.Entity<Employee>().HasIndex(p => p.Phone_No).IsUnique();
             builder.Entity<Employee>().Property(p => p.Email).HasColumnType("varchar(30)").HasMaxLength(30).IsRequired();
+            builder.Entity<Employee>().HasIndex(p => p.Email).IsUnique();
             builder.Entity<Employee>().Property(p => p.Gender).HasColumnType("varchar(1)").HasMaxLength(1).IsRequired();
             builder.Entity<Employee>().HasOne(p => p.Position).WithMany(p => p.Employees).HasForeignKey(p => p.Position_Code).IsRequired();
             builder.Entity<Employee>().Property(p => p.DOB).HasColumnType("date").HasMaxLength(16).IsRequired();
             builder.Entity<Employee>().Property(p => p.Salary).HasColumnType("decimal(16)").HasMaxLength(16).IsRequired();
             builder.Entity<Employee>().Property(p => p.Remarks).HasColumnType("varchar(1000)").HasMaxLength(1000);
             builder.Entity<Employee>().Property(p => p.Photo).HasColumnType("varchar(30)").HasMaxLength(30).IsRequired();
+            builder.Entity<Employee>().HasIndex(p => p.Photo).IsUnique();
 
-            
         }
     }
 }
