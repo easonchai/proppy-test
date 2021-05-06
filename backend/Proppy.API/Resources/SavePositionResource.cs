218fcd115aa7a7f9009349b1c5144254640f3eb1
Eason Chai <echai2905@gmail.com>
2021-05-06 08:41:12 +0800

    Update Post Resource

--- backend/Proppy.API/Resources/SavePositionResource.cs
+++ backend/Proppy.API/Resources/SavePositionResource.cs
@@ -5,6 +5,10 @@ namespace Proppy.API.Resources
     public class SavePositionResource
     {
         [Required]
+        [MaxLength(2)]
+        public string Code {get; set;}
+
+        [Required]
         [MaxLength(20)]
         public string Description {get; set;}
     }
