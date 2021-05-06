269cd0398ef09930c60bbee2c9f78d7d23fec0cd
Eason Chai <echai2905@gmail.com>
2021-05-06 19:24:08 +0800

    Add Post Route

--- backend/Proppy.API/Controllers/EmployeeController.cs
+++ backend/Proppy.API/Controllers/EmployeeController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Proppy.API.Domain.Models;
 using Proppy.API.Domain.Services;
 using Proppy.API.Resources;
+using Proppy.API.Extensions;
 
 namespace Proppy.API.Controllers
 {
@@ -30,14 +31,37 @@ namespace Proppy.API.Controllers
         }
 
         [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] SaveEmployeeResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+
+            var employee = _mapper.Map<SaveEmployeeResource, Employee>(resource);
+            var result = await _employeeService.SaveAsync(employee);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            var employeeResource = _mapper.Map<Employee, EmployeeResource>(result.Employee);
+            return Ok(employeeResource);
+        }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateAsync(int id, [FromBody])
+        public async Task<IActionResult> PutAsync(int id, [FromBody])
         {
             var employees = await _employeeService.ListAsync();
             var resources = _mapper.Map<IEnumerable<Employee>, IEnumerable<EmployeeResource>>(employees);
 
-            return resources;
+            
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAsync(int id, [FromBody])
+        {
+            var employees = await _employeeService.ListAsync();
+            var resources = _mapper.Map<IEnumerable<Employee>, IEnumerable<EmployeeResource>>(employees);
+
+            
         }
     }
 }
