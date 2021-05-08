06fc6bbd73e54230796225b829c920a55221052f
Eason Chai <echai2905@gmail.com>
2021-05-08 11:04:10 +0800

    Add Preliminary FilterOptions

--- app/src/components/FilterOptions.vue
+++ app/src/components/FilterOptions.vue
@@ -1,16 +1,71 @@
 <template>
   <ion-content class="ion-padding">
-    Popover Content
+    <div class="filter__option">
+      <ion-label>Items Per Page</ion-label>
+      <ion-select placeholder="Select One" v-model="itemsPerPage">
+        <ion-select-option value="10">10</ion-select-option>
+        <ion-select-option value="20">20</ion-select-option>
+        <ion-select-option value="50">50</ion-select-option>
+        <ion-select-option value="100">100</ion-select-option>
+      </ion-select>
+    </div>
+    <div class="filter__option">
+      <ion-label>Sort By</ion-label>
+      <ion-select placeholder="Select One" v-model="sortBy">
+        <ion-select-option value="asc">Ascending</ion-select-option>
+        <ion-select-option value="desc">Descending</ion-select-option>
+      </ion-select>
+    </div>
+    <div class="filter__option">
+      <ion-label>Filter By</ion-label>
+      <ion-select placeholder="Select One" v-model="filterBy">
+        <ion-select-option value="10">10</ion-select-option>
+        <ion-select-option value="20">20</ion-select-option>
+        <ion-select-option value="50">50</ion-select-option>
+        <ion-select-option value="100">100</ion-select-option>
+      </ion-select>
+    </div>
   </ion-content>
 </template>
 
 <script>
-import { IonContent } from "@ionic/vue";
+import { IonContent, IonLabel, IonSelect, IonSelectOption } from "@ionic/vue";
+import { watch, ref } from "vue";
 
 export default {
-  name: "Popover",
-  components: { IonContent },
+  name: "FilterOptions",
+  components: { IonContent, IonLabel, IonSelect, IonSelectOption },
+  emits: ["itemsPerPage", "sortBy", "filterBy"],
+  setup(props, { emit }) {
+    const itemsPerPage = ref("");
+    const sortBy = ref("");
+    const filterBy = ref("");
+
+    watch(itemsPerPage, (newVal) => {
+      emit("itemsPerPage", { newVal });
+    });
+
+    watch(sortBy, (newVal) => {
+      emit("sortBy", { newVal });
+    });
+
+    watch(filterBy, (newVal) => {
+      emit("filterBy", { newVal });
+    });
+
+    return {
+      itemsPerPage,
+      sortBy,
+      filterBy,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.filter__option {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 12px;
+}
+</style>
