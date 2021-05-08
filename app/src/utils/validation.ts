53232bdc456d188d34ad7cdd348b76e0a24868c1
Eason Chai <echai2905@gmail.com>
2021-05-08 18:43:29 +0800

    Add Regex For Validation

---
+++ app/src/utils/validation.ts
@@ -0,0 +1,16 @@
+const regex = {
+  EMAIL_REGEX: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  PHONE_NUMBER_REGEX: /^[+][0-9]+[\b]*$/,
+};
+
+export function validateEmail(email: string) {
+  const emailRegex = regex.EMAIL_REGEX;
+  if (emailRegex.test(email)) return true;
+  return false;
+}
+
+export function validatePhone(phone: string) {
+  const phoneRegex = regex.PHONE_NUMBER_REGEX;
+  if (phoneRegex.test(phone)) return true;
+  return false;
+}
