b8bcc9b04ce6f6911e6c7b983c7c354c0ec274fd
Eason Chai <echai2905@gmail.com>
2021-05-08 18:36:35 +0800

    Add Create Employee Form

--- app/src/components/EmployeeForm.vue
+++ app/src/components/EmployeeForm.vue
@@ -2,36 +2,26 @@
   <form class="form__container" @submit.prevent>
     <ion-item class="input__field">
       <ion-label position="floating">Employee Name</ion-label>
-      <ion-input type="text" required v-model="name"></ion-input>
+      <ion-input type="text" required></ion-input>
     </ion-item>
     <ion-item class="input__field">
       <ion-label position="floating">Phone No</ion-label>
-      <ion-input type="phone" required v-model="phoneNo"></ion-input>
+      <ion-input type="phone" required></ion-input>
     </ion-item>
     <ion-item class="input__field">
       <ion-label position="floating">Email</ion-label>
-      <ion-input type="email" required v-model="email"></ion-input>
+      <ion-input type="email" required></ion-input>
     </ion-item>
     <ion-item class="input__field">
       <ion-label>Gender</ion-label>
-      <ion-select
-        placeholder="Select One"
-        v-model="gender"
-        interface="popover"
-        :interface-options="options"
-      >
+      <ion-select placeholder="Select One">
         <ion-select-option value="F">Female</ion-select-option>
         <ion-select-option value="M">Male</ion-select-option>
       </ion-select>
     </ion-item>
     <ion-item class="input__field">
       <ion-label>Position</ion-label>
-      <ion-select
-        placeholder="Select One"
-        v-model="positionCode"
-        interface="popover"
-        :interface-options="options"
-      >
+      <ion-select placeholder="Select One">
         <ion-select-option value="A">Senior Manager</ion-select-option>
         <ion-select-option value="B">Manager</ion-select-option>
         <ion-select-option value="C">Engineer</ion-select-option>
@@ -44,81 +34,37 @@
     </ion-item>
     <ion-item class="input__field">
       <ion-label position="floating">Salary (RM)</ion-label>
-      <ion-input type="number" required v-model="salary"></ion-input>
+      <ion-input type="decimal" required></ion-input>
     </ion-item>
     <ion-item class="input__field">
       <ion-label position="floating">Image URL</ion-label>
-      <ion-input type="text" required v-model="photo"></ion-input>
+      <ion-input type="text" required></ion-input>
     </ion-item>
     <ion-item class="input__field">
       <ion-label position="floating">Remarks</ion-label>
-      <ion-textarea
-        placeholder="Enter employee remarks..."
-        v-model="remarks"
-      ></ion-textarea>
+      <ion-textarea placeholder="Enter employee remarks..."></ion-textarea>
     </ion-item>
-    <ion-button expand="block" :disabled="buttonDisabled"
-      >Add Employee</ion-button
-    >
+    <ion-button expand="block">Add Employee</ion-button>
   </form>
 </template>
 
 <script>
-import {
-  IonInput,
-  IonItem,
-  IonLabel,
-  IonTextarea,
-  IonButton,
-  IonSelectOption,
-  IonSelect,
-} from "@ionic/vue";
+import { IonInput, IonItem, IonLabel } from "@ionic/vue";
 
 export default {
-  components: {
-    IonInput,
-    IonItem,
-    IonLabel,
-    IonTextarea,
-    IonButton,
-    IonSelectOption,
-    IonSelect,
-  },
-  name: "AddEmployee",
+  components: { IonInput, IonItem, IonLabel },
+  name: "EmployeeForm",
   data() {
     return {
       name: "",
       email: "",
       phoneNo: "",
       gender: "M",
-      positionCode: "A",
+      position: "A",
       salary: 0,
-      photo: "",
+      imageUrl: "",
       remarks: "",
-      emailError: false,
-      phoneError: false,
-      salaryError: false,
     };
-  },
-  computed: {
-    buttonDisabled() {
-      if (
-        !this.name ||
-        this.emailError ||
-        this.phoneError ||
-        !this.gender ||
-        !this.positionCode ||
-        this.salaryError ||
-        !this.photo
-      )
-        return true;
-      return false;
-    },
-  },
-  methods: {
-    createEmployee() {
-      console.log("create");
-    },
   },
 };
 </script>
@@ -132,11 +78,5 @@ export default {
 
 .input__field {
   margin-bottom: 14px;
-}
-
-/* Popover Interface: set color for the popover using Item's CSS variables */
-.styled_select .select-interface-option {
-  --color: #333;
-  --color-hover: #333;
 }
 </style>
